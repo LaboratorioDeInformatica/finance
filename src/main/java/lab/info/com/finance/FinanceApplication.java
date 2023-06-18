56520f259a8e50c6ce1d0c0e69fef8610c1c9c7b
LaboratorioDeInformatica <labdeinf@gmail.com>
2023-06-18 16:51:44 -0300

    cors configuration

--- src/main/java/lab/info/com/finance/FinanceApplication.java
+++ src/main/java/lab/info/com/finance/FinanceApplication.java
@@ -2,9 +2,18 @@ package lab.info.com.finance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class FinanceApplication {
+@EnableWebMvc
+public class FinanceApplication implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceApplication.class, args);
